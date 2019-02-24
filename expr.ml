ce0a393ab748d91657c653212018fc2d401afa0d
Richard Cook <rcook@rcook.org>
2019-02-23 20:57:21 -0800

    Use result instead of option

--- expr.ml
+++ expr.ml
@@ -1,4 +1,4 @@
-open Core.Option
+open Core.Result
 open Printf
 
 type expr =
@@ -20,31 +20,31 @@ let rec to_sexpr = function
         sprintf "(div %s %s)" (to_sexpr left) (to_sexpr right)
 
 let rec eval = function
-    | Val x -> Some x
+    | Val x -> Ok x
     | Add (left, right) -> binOp left right (+)
     | Sub (left, right) -> binOp left right (-)
     | Mul (left, right) -> binOp left right ( * )
     | Div (left, right) -> binOpM left right (fun l r ->
         if r = 0
-            then None
-            else Some (l / r))
-and binOp left right f = map2 (eval left) (eval right) f
+            then Error "Divide by zero"
+            else Ok (l / r))
+and binOp left right f = binOpM left right (fun l r -> Ok (f l r))
 and binOpM left right f =
     eval left >>= fun l ->
     eval right >>= fun r ->
     f l r
 
-let string_of_option f o = match o with
-    | None -> "None"
-    | Some x -> sprintf "Some %s" (f x)
+let string_of_result f r = match r with
+    | Error m -> sprintf "Error %s" m
+    | Ok x -> sprintf "Some %s" (f x)
 
 let () =
     (* Divide by 2 *)
     let e = Div (Mul (Val 2, Add (Val 3, Val 4)), Val 2) in
     print_endline (to_sexpr e);
-    print_endline (string_of_option string_of_int (eval e));
+    print_endline (string_of_result string_of_int (eval e));
 
     (* Divide by 0 *)
     let e = Div (Mul (Val 2, Add (Val 3, Val 4)), Val 0) in
     print_endline (to_sexpr e);
-    print_endline (string_of_option string_of_int (eval e));
+    print_endline (string_of_result string_of_int (eval e));
